f5cd2a3fa883ad1d2c2183233dd84bc1d43e7e2b
Sandy Chen <sandy1990418@gmail.com>
2024-08-16 03:24:56 +0000

    Feat: add FastAPI implementation for easier and more accessible usage

--- app/docker.sh
+++ app/docker.sh
@@ -10,3 +10,14 @@ docker ps -a
 docker build -t asr-service .
 docker run -d --name asr-container --gpus all -p 8000:8000 --env-file .env asr-service
 docker exec -it 536549e721d0 bash
+
+docker container run -d -it -d --privileged=true --name whispercpp --gpus all    ghcr.io/ggerganov/whisper.cpp:main-cuda  /bin/bash
+f8a2d6fc0ae9
+
+
+## You should be able to run the following commands in the container
+# Find CUDA bin path and export it
+apt-get update
+apt-get install -y build-essential python3 git make cmake libsdl2-dev
+make clean
+GGML_CUDA=1 make -j stream
