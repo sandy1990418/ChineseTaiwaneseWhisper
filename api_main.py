81812aefcc3571887691b4cb2ede8e6e9bcc8b66
Sandy Chen <sandy1990418@gmail.com>
2024-08-19 14:07:58 +0000

    chore: add docker.sh file and start.sh to control docker container and image

--- api_main.py
+++ api_main.py
@@ -117,4 +117,4 @@ async def health_check():
 #     import uvicorn
 
 #     logger.info("Starting FastAPI application on port %s", os.getenv("PORT", 8000))
-#     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+#     uvicorn.run(app, host="0.0.0.0", port=8000))
---
+++ app/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -m fastapi run api_main.py --host 0.0.0.0 --port 8000
