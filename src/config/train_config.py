eb8a7052c6f7b4b4c96fc6cc23b6dffd9284e86b
Sandy Chen <sandy1990418@gmail.com>
2024-10-11 07:25:19 +0000

    chore: change the steps setting in  train_config

--- src/config/train_config.py
+++ src/config/train_config.py
@@ -95,15 +95,15 @@ class WhisperTrainingArguments(Seq2SeqTrainingArguments):
         metadata={"help": "The saving strategy to use."}
     )
     save_steps: int = field(
-        default=0.01,
+        default=100,
         metadata={"help": "Save checkpoint every X updates steps."}
     )
     eval_steps: int = field(
-        default=0.01,
+        default=100,
         metadata={"help": "Run an evaluation every X steps."}
     )
     logging_steps: int = field(
-        default=0.01,
+        default=100,
         metadata={"help": "Log every X updates steps."}
     )
     save_total_limit: Optional[int] = field(
